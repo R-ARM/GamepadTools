bc9bb57d08f877578d3b6ddafc9e3470fd21e580
Maccraft123 <maccraft123mc@gmail.com>
2022-08-15 15:15:03 +0200

    switch to uefi-rs from custom entry stuff

--- bootmgr/src/main.rs
+++ bootmgr/src/main.rs
@@ -1,17 +1,17 @@
+#![feature(ptr_metadata)]
 use gamepad_gui::ToolkitBuilder;
+use std::ffi::c_void;
 use efivar::efi::{VariableName, VariableFlags};
+
+use uefi::proto::device_path::DevicePath;
+use uefi::proto::device_path::DevicePathInstance;
+
+use uefi::proto::device_path::DevicePathNode;
+use uefi::proto::device_path::DevicePathHeader;
 use std::str::FromStr;
 use std::io::BufRead;
 use regex::Regex;
-
-static DESCRIPTION_OFFSET_BYTES: usize = (32+16)/8;
-
-#[derive(Debug)]
-struct Entry {
-    id: u16,
-    description: String,
-    path: Vec<String>,
-}
+use core::{mem, ptr};
 
 fn char16_to_string(buf: &[u8]) -> (String, usize) {
     let mut iter = buf.iter();
@@ -38,105 +38,57 @@ fn char16_to_string(buf: &[u8]) -> (String, usize) {
         .collect::<String>(), i)
 }
 
+#[derive(Debug)]
+struct Entry {
+    id: u16,
+    id_string: String,
+    description: String,
+    path: Vec<String>,
+}
 
 impl Entry {
-    fn new(var: &str, buf: &[u8]) -> Option<Entry> {
-        let (description, end) = char16_to_string(&buf[DESCRIPTION_OFFSET_BYTES..]);
-        let desc_end_offset = DESCRIPTION_OFFSET_BYTES + end;
+    fn new(var: &str, buf: &[u8]) -> Self {
+        let (description, end) = char16_to_string(&buf[(32+16)/8..]);
+        let desc_end_offset = (32+16)/8 + end;
+        let device_path: &DevicePath = unsafe {
+            std::mem::transmute(&buf[desc_end_offset..])
+        };
 
-        let mut path = Vec::<String>::new();
-        let pathlist: &[u8] = &buf[desc_end_offset..];
-        let cur_start: usize = 0;
-        let mut cur: usize = cur_start;
-
-        loop {
-            let cur_type = pathlist[cur];
-            let subtype = pathlist[cur+1];
-            let len = pathlist[cur+2];
-
-            //println!("type {:X?} subtype {:X?} len {:X?}", cur_type, subtype, len);
-            match cur_type {
-                0x03 => { // Messaging Device Type
-                    match subtype {
-                        01 => path.push("ATAPI".to_string()),
-                        02 => path.push("SCSI".to_string()),
-                        03 | 21 => path.push("Fibre Channel".to_string()),
-                        04 => path.push("1394".to_string()),
-                        05 | 15 | 16 => path.push("USB".to_string()),
-                        06 => path.push("I2O".to_string()),
-                        09 | 11 | 12 | 13 | 20 | 28 | 31
-                            => path.push("Network".to_string()),
-                        10 => path.push("Vendor specific".to_string()),
-                        17 => (), // don't care
-                        18 => path.push("SATA".to_string()),
-                        19 => path.push("iSCSI".to_string()),
-                        22 => path.push("SAS".to_string()),
-                        23 => path.push("NVMe".to_string()),
-                        24 => path.push("URI".to_string()),
-                        25 => path.push("UFS".to_string()),
-                        26 => path.push("SD Card".to_string()),
-                        27 | 30 => path.push("Bluetooth".to_string()),
-                        29 => path.push("eMMC".to_string()),
-                        32 => path.push("NVDIMM".to_string()),
-                        _ => unreachable!("Corrupted data or future spec"),
-                    }
-                }
-                0x04 => { // Media Device Path
-                    match subtype {
-                        01 => path.push("Hard Drive".to_string()),
-                        02 => path.push("CD-ROM".to_string()),
-                        03 => todo!("Vendor-defined Media Device Path subtype isn't handled!"),
-                        04 => {
-                            let (tmp, _) = char16_to_string(&pathlist[cur+4..]);
-                            path.push(tmp);
-                        },
-                        06 | 07 => path.push("UEFI PI".to_string()),
-                        _ => {
-                            todo!("{} Media Device Path subtype not supported", subtype);
-                        }
-                    }
-                },
-                0x05 => path.push("CSM".to_string()),
-                0x7F => { // End of Hardware Device Path
-                    let tmp = if subtype == 0xFF {
-                        "End Entire Device Path"
-                    } else {
-                        "End This Instance of a Device Path and start a new Device Path"
-                    };
-
-                    println!("Wrapping up... found {} node", tmp);
-                    break;
-                }
-                _ => {
-                    todo!("{:X?} Device Path Node type not supported", cur_type);
-                }
-            }
-
-            // advance our "pointer"
-            cur += len as usize;
-
-            if cur >= buf.len() {
-                break;
+        let mut out_path: Vec<String> = Vec::new();
+        for node in device_path.node_iter() {
+            if let Some(file) = node.as_file_path_media_device_path() {
+                let path = file.path_name().to_cstring16().unwrap();
+                out_path.push(path.to_string());
+            } else {
+                out_path.push(format!("{:?}", node.device_type()));
             }
         }
-
-        for i in &path {
-            println!("{}", i);
-        }
-
 
         let boot_id = var.to_string().split_off(4);
         let id = if let Ok(tmp) = u16::from_str_radix(&boot_id, 16) {
             tmp
         } else {
-            return None
+            0
         };
 
-        Some(Entry {
-            path,
+        Entry {
             id,
+            id_string: boot_id,
             description,
-        })
+            path: out_path,
+        }
+    }
+}
+
+impl std::fmt::Display for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut path = String::new();
+        for tmp in &self.path {
+            path.push_str(&tmp);
+            path.push_str(" ");
+        }
+        path.pop(); // remove trailing space
+        write!(f, "Boot{}: {}, at: '{}'", self.id_string, self.description, path)
     }
 }
 
@@ -157,41 +109,20 @@ fn main() -> Result<(), efivar::Error> {
 
     let boot_xxxx = Regex::new(r"^Boot\d\d\d\d$").unwrap();
     let mut manager = efivar::system();
-    let mut buf = vec![0u8; 512];
-    let mut options: Vec<Entry> = Vec::new();
+    let mut buf: [u8; 256] = [0u8; 256];
+    //let mut options: Vec<DevicePath> = Vec::new();
 
     for var in manager.get_var_names().expect("asdf") {
         if boot_xxxx.is_match(var.variable()) {
             match manager.read(&var, &mut buf)  {
                 Ok((size, ..)) => {
-                    if let Some(entry) = Entry::new(&var.short_name(), &buf[..size]) {
-                        options.push(entry);
-                    }
+                    let tmp = Entry::new(var.variable(), &buf);
+                    println!("{}", tmp);
+                    
                 }
                 Err(e) => eprintln!("{}", e),
             }
         }
-    }
-
-    options.sort_by_key(|s| s.id);
-
-    for opt in &options {
-        println!("Choose next boot: {}: {}, at: {:?}", opt.id, opt.description, opt.path);
-    }
-
-    let stdin = std::io::stdin();
-    let input = stdin.lock().lines().next().unwrap().unwrap();
-    let tmp: u16 = u16::from_str(&input).unwrap();
-    if let Some(choice) = options.iter().find(|s| s.id == tmp) {
-        println!("Setting {}", choice.description);
-
-        let next = VariableName::new("BootNext");
-        let attr = VariableFlags::NON_VOLATILE | VariableFlags::BOOTSERVICE_ACCESS | VariableFlags::RUNTIME_ACCESS;
-        let val: [u8; 2] = choice.id.to_le_bytes();
-
-        //manager.write(&next, attr, &val)?;
-    } else {
-        eprintln!("wrong choice");
     }
     Ok(())
 }
